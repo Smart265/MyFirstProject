b270d0c06a0955ac05669aa601f931f8ef082ccc
Smart265 <Bsc-inf-08-23@unima.ac.mw>
2025-04-01 15:13:43 +0200

    Update Random.cpp
    Just want to update some of the statement in an existing code

--- lab/Random.cpp
+++ lab/Random.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
+#include <cstdlib>
+#include <ctime> 
 int main ()
 {
+    srand(time(0));
     int DaysUntilExpiration = rand() % 12;
-    cout << "Enter the random numbers." <<endl;
-    if (DaysUntilExpiration <= 10) {
-        cout << "Your subscription will expire soon. Renew now!" <<endl;
+    cout << "Days until subscription expires: " << DaysUntilExpiration << endl;
+    if (DaysUntilExpiration == 0) {
+        cout << "Your subscription has expired. Renew it!" <<endl;
     }
-    if (DaysUntilExpiration <= 5) {
-        cout << "Your subscription expires in " <<"DaysUntilExpirationDays"<< endl; 
-        cout << "Renew now and save 10%!" << endl;
+    else if (DaysUntilExpiration == 1) {
+        cout << "Your subscription expires within a day!" 
+        "\nRenew now and save 20%!" << endl;
+    }
+    else if (DaysUntilExpiration <= 5){
+        cout << "Your subscription expires in " << DaysUntilExpiration << "days.\nRenew now and save 10%!" <<endl;
+    }
+    else if (DaysUntilExpiration <= 10){
+        cout << "Your subscription will expires soon. renew now!" << endl;
+    }
+    else {
+        cout << "You ave an active subscription." << endl;
     }
     return 0;
 } 
